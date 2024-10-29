25f516f6a03d2ebfa2438507970b2717cc66d726
MaximilianoGarin <m.garnvelsquez@uandresbello.edu>
2024-10-28 21:21:08 -0300

    update app.js

--- client/src/app.js
+++ client/src/app.js
@@ -15,13 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('MongoDB connection error:', err));
-
+mongoose.connect("mongodb+srv://MaxiGarin21:Maxi67924@proyectodw.gip16.mongodb.net/",{useNewUrlParser: true, useUnifiedTopology: true});
 // Routes
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the Food Ticket System API' });
